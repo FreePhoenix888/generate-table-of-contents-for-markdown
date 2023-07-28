7c870779b57b0a24d87dc4b0c75362d84c3961dc
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 08:29:49 +0600

    Remove stdui

--- automation/documentation/generate.ts
+++ automation/documentation/generate.ts
@@ -5,13 +5,13 @@ import { glob } from 'glob';
 generate()
 
 async function generate() {
-  const { stdout: username } = await execa('git', ['config', '--global', 'user.name'], {reject: false, stdio: 'overlapped', verbose: true});
+  const { stdout: username } = await execa('git', ['config', '--global', 'user.name'], {reject: false,  verbose: true});
   if (!username) {
-    await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888'], {stdio: 'overlapped', verbose: true});
+    await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888'], { verbose: true});
   }
-  const { stdout: email } = await execa('git', ['config', '--global', 'user.email'], {reject: false, stdio: 'overlapped', verbose: true});
+  const { stdout: email } = await execa('git', ['config', '--global', 'user.email'], {reject: false,  verbose: true});
   if (!email) {
-    await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com'], {stdio: 'overlapped', verbose: true});
+    await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com'], { verbose: true});
   }
 
   const files = await glob('./dist/cli/*.js');
@@ -31,50 +31,49 @@ async function generate() {
   const cliUsageWays = await execa('npx', ['@freephoenix888/generate-usage-ways-of-npm-cli-apps-in-markdown-format', '--root-header-level', '2']);
   readmeContents = readmeContents.replace(/(?<start><!-- CLI_USAGE_WAYS_START -->)[\S\s]*(?<end><!-- CLI_USAGE_WAYS_END -->)/g, `$<start>\n${cliUsageWays.stdout}\n$<end>`);
 
-  const { stdout: tableOfContents } = await execa('node', [`./dist/cli/generate-table-of-contents-for-markdown.js`,`--markdown-file-path`, readmeFilePath], {stdio: 'overlapped', verbose: true});
+  const { stdout: tableOfContents } = await execa('node', [`./dist/cli/generate-table-of-contents-for-markdown.js`,`--markdown-file-path`, readmeFilePath], { verbose: true});
   readmeContents = readmeContents.replace(/(?<start><!-- ACTUAL_TABLE_OF_CONTENTS_START -->)[\S\s]*(?<end><!-- ACTUAL_TABLE_OF_CONTENTS_END -->)/g, `$<start>\n${tableOfContents}\n$<end>`);
 
   await fsExtra.writeFile(readmeFilePath, readmeContents);
 
-  await execa(`git`, ['add', readmeFilePath], {stdio: 'overlapped', verbose: true});
-  const execResultAfterReadmeUpdate = await execa('git', ['diff', '--staged', '--quiet'], { reject: false , stdio: 'overlapped', verbose: true});
+  await execa(`git`, ['add', readmeFilePath], { verbose: true});
+  const execResultAfterReadmeUpdate = await execa('git', ['diff', '--staged', '--quiet'], { reject: false ,  verbose: true});
   if (execResultAfterReadmeUpdate.exitCode === 0) {
     console.log("No changes to commit");
   }
   else {
-    await execa('git', ['commit', '-m', 'Update README.md'], {stdio: 'overlapped', verbose: true});
-    await execa('git', ['push', 'origin', 'main'], {stdio: 'overlapped', verbose: true});
+    await execa('git', ['commit', '-m', 'Update README.md'], { verbose: true});
+    await execa('git', ['push', 'origin', 'main'], { verbose: true});
   }
 
   // Generate the docs first
-  await execa('npx', ['typedoc', './src/main.ts'], {stdio: 'overlapped', verbose: true});
+  await execa('npx', ['typedoc', './src/main.ts'], { verbose: true});
 
   // Stage and commit the docs in the main branch
-  await execa('git', ['add', 'docs'], {stdio: 'overlapped', verbose: true});
-  await execa('git', ['commit', '-m', 'Update documentation'], {stdio: 'overlapped', verbose: true});
+  await execa('git', ['add', 'docs'], { verbose: true});
+  await execa('git', ['commit', '-m', 'Update documentation'], { verbose: true});
 
-  await execa('git', ['fetch', 'origin'], {stdio: 'overlapped', verbose: true});
+  await execa('git', ['fetch', 'origin'], { verbose: true});
   // Check if the gh-pages branch exists
-  const { stdout: ghPagesBranchExists } = await execa('git', ['branch', '-r', '--list', 'origin/gh-pages'], {reject: false, stdio: 'overlapped', verbose: true});
-  console.log({ghPagesBranchExists})
+  const { stdout: ghPagesBranchExists } = await execa('git', ['branch', '-r', '--list', 'origin/gh-pages'], {reject: false,  verbose: true});
 
   if (!ghPagesBranchExists) {
     // If it doesn't exist, create it as an orphan branch
-    await execa('git', ['checkout', '--orphan', 'gh-pages'], {stdio: 'overlapped', verbose: true});
+    await execa('git', ['checkout', '--orphan', 'gh-pages'], { verbose: true});
   } else {
     // If it does exist, just checkout to it
-    await execa('git', ['checkout', 'gh-pages'], {stdio: 'overlapped', verbose: true});
+    await execa('git', ['checkout', 'gh-pages'], { verbose: true});
   }
 
   // Checkout the docs from the main branch to the gh-pages branch
-  await execa('git', ['checkout', 'main', '--', 'docs'], {stdio: 'overlapped', verbose: true});
+  await execa('git', ['checkout', 'main', '--', 'docs'], { verbose: true});
 
   // Commit and push the changes
-  await execa('git', ['commit', '-m', 'Update documentation'], {stdio: 'overlapped', verbose: true});
-  await execa('git', ['push', 'origin', 'gh-pages'], {stdio: 'overlapped', verbose: true});
+  await execa('git', ['commit', '-m', 'Update documentation'], { verbose: true});
+  await execa('git', ['push', 'origin', 'gh-pages'], { verbose: true});
 
   // Switch back to the main branch
-  await execa('git', ['checkout', 'main'], {stdio: 'overlapped', verbose: true});
+  await execa('git', ['checkout', 'main'], { verbose: true});
   
 
 };
