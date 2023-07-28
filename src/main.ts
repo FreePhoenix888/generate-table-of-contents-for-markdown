4073050784355bfd1cf150b987fa59e6bb7f8f62
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 10:39:27 +0600

    Add js to import path

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-export { generateTableOfContentsForMarkdown, type  GenerateTableOfContentsForMarkdownOptions } from "./generate-table-of-contents-for-markdown";
+export { generateTableOfContentsForMarkdown, type  GenerateTableOfContentsForMarkdownOptions } from "./generate-table-of-contents-for-markdown.js";
